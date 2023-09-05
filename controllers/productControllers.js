5e57f0a8d015f8687b6795964ec1d7c84ab68765
Deepak.malik-at-527579388740 <Deepak.malik-at-527579388740>
2023-09-05 17:33:22 +0530

    all query filters added

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -9,8 +9,41 @@ const populateDB = async (req, res) => {
   }
 };
 
-const getAllProducts = (req, res) => {
-  res.send("all products");
+const getAllProducts = async (req, res) => {
+  const queryObject = {}; //to just pass the fields that we want to filter and ignore rest of the query keys
+
+  const { featured, company, name, sort } = req.query;
+  if (featured) {
+    queryObject.featured = Boolean(featured);
+  }
+
+  if (company) {
+    queryObject.featured = featured;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, option: i };
+  }
+
+  let result = Product.find(queryObject); //not using await to chain sort later
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 5;
+  console.log(page);
+  const skip = (page - 1) * limit;
+  console.log(skip);
+
+  result = result.skip(skip).limit(limit);
+  const products = await result;
+
+  res.status(200).json({ products });
 };
 
 module.exports = { populateDB, getAllProducts };
